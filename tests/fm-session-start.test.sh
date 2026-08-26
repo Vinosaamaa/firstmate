10bce25e278b61d6655fd10956ed57a3a7c154ba
wenkxu <v-wenkxu@expediagroup.com>
2026-08-26 16:57:40 -0700

    test: bind session-start fixture to actual harness pid

--- tests/fm-session-start.test.sh
+++ tests/fm-session-start.test.sh
@@ -578,11 +578,12 @@ EOF
 }
 
 run_session_start_secondmate() {
-  local root=$1 home=$2 fakebin=$3 mate=$4 log=$5 spawned=$6 mode=$7
+  local root=$1 home=$2 fakebin=$3 mate=$4 log=$5 spawned=$6 mode=$7 harness_pid
+  harness_pid=$(sh -c 'printf "%s\n" "$PPID"')
   TMUX='' FM_BACKEND=tmux FM_FAKE_TMUX_MODE="$mode" FM_FAKE_TMUX_LOG="$log" \
     FM_FAKE_TMUX_SPAWNED="$spawned" FM_FAKE_SECOND_MATE_HOME="$mate" \
     FM_FAKE_SECOND_MATE_ID="$SESSION_START_SECOND_MATE_ID" \
-    FM_FAKE_HARNESS_PID=$$ \
+    FM_FAKE_HARNESS_PID="$harness_pid" \
     run_session_start "$home" "$root" "$fakebin:$BASE_PATH"
 }
 
